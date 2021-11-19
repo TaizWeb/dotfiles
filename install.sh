ea6471ac051f8c3d4f85e5a0f2e69d7a36c44138
taizweb <tyler.walker.student@gmail.com>
2021-11-19 15:06:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,12 +12,16 @@ ln -s ./.bashrc ~/.bashrc
 # Linking input config
 ln -s ./.inputrc ~/.inputrc
 
+# Linking vimrc
+ln -s ./.vimrc ~/.vimrc
+
 # Linking git config and adding it to git
 ln -s ./.gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 
 # Linking the terminalrc
-ln -s ~/xfce/terminalrc ~/.config/xfce/terminal/terminalrc
+# I notice this alternates between using xfce and xfce4?
+ln -s ~/xfce/terminalrc ~/.config/xfce4/terminal/terminalrc
 
 # Printing a success message
 echo 'Success! To finish up, run :VundleInstall within vim!'
