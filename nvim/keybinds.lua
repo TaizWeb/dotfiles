a73db075fe8db4018259080c82abbd14fa6addc2
taiz <walter.kerly@protonmail.com>
2024-01-11 21:26:06 -0500

    feat(keybings): Add bindings for ctrl-arrow to                 navigate panes

--- nvim/keybinds.lua
+++ nvim/keybinds.lua
@@ -23,6 +23,12 @@ vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true 
 vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
 
+-- And CTRL-arrow
+vim.api.nvim_set_keymap("n", "<C-Left>", "<C-w>h", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-Down>", "<C-w>j", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-Up>", "<C-w>k", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-Right>", "<C-w>l", { noremap = true, silent = true })
+
 -- Telescope bindings
 local builtin = require("telescope.builtin")
 
