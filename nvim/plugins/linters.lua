2617e32241ae2c70ee97b9870493577ae6890108
taiz <walter.kerly@protonmail.com>
2024-07-04 01:52:45 -0400

    feat(nvim): Add more linting for lua

--- nvim/plugins/linters.lua
+++ nvim/plugins/linters.lua
@@ -1,5 +1,13 @@
 -- Linter File types
 require("lint").linters_by_ft = {
+	-- lua = {
+	-- 	{
+	-- 		cmd = "luacheck",
+	-- 		args = { "--globals", "vim", "love" },
+	-- 		stdin = true,
+	-- 	},
+	-- },
+	lua = { "luacheck", args = { "--globals ", "vim" } },
 	lua = { "luacheck" },
 	python = { "pylint" },
 	typescriptreact = { "eslint_d" },
