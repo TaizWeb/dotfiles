42ce1667588d4ed2bc3bb837e9e614af06155904
taiz <walter.kerly@protonmail.com>
2024-07-29 21:30:16 -0400

    Use mouse with hardtime

--- nvim/plugins/lazy-plugins.lua
+++ nvim/plugins/lazy-plugins.lua
@@ -131,7 +131,7 @@ local plugins = {
 	{
 		"m4xshen/hardtime.nvim",
 		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-		opts = {},
+		opts = { disable_mouse = false },
 	},
 }
 
