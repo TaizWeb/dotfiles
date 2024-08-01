ca729745718f2eceb0c81f70e3b0a245cf562ea2
taiz <walter.kerly@protonmail.com>
2024-08-01 18:50:04 -0400

    Remove luacheck, since the LSP handles that already

--- nvim/plugins/linters.lua
+++ nvim/plugins/linters.lua
@@ -1,21 +1,5 @@
 -- Linter File types
 local lint = require("lint")
--- local luacheck = lint.linters.luacheck
--- luacheck.args = {
--- 	"--globals",
--- 	"love",
--- 	"-",
--- }
-
 lint.linters_by_ft = {
-	-- lua = {
-	-- 	{
-	-- 		cmd = "luacheck",
-	-- 		args = { "--globals", "vim", "love" },
-	-- 		stdin = true,
-	-- 	},
-	-- },
-	lua = { cmd = "luacheck", args = { "--globals ", "vim" } },
-	lua = { "luacheck" },
 	typescriptreact = { "eslint_d" },
 }
