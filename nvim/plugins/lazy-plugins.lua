bd5ae6dff76c4d5f5068423b48b3c7cd61d26a15
taiz <walter.kerly@protonmail.com>
2024-08-06 18:50:46 -0400

    Add LSP support for nvim api

--- nvim/plugins/lazy-plugins.lua
+++ nvim/plugins/lazy-plugins.lua
@@ -127,11 +127,36 @@ local plugins = {
 		end,
 	},
 
-	-- Encourage me to "git gud"
+	-- NeoVim API Support
 	{
-		"m4xshen/hardtime.nvim",
-		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-		opts = { disable_mouse = false },
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{ "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+	{ -- optional completion source for require statements and module annotations
+		"hrsh7th/nvim-cmp",
+		opts = function(_, opts)
+			opts.sources = opts.sources or {}
+			table.insert(opts.sources, {
+				name = "lazydev",
+				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+			})
+		end,
+		-- { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
+	},
+
+	-- Personal plugin testing
+	{
+		dir = "~/Projects/buffwipe", -- Use the full path to your plugin
+		-- config = function() end,
+		opts = {},
 	},
 }
 
