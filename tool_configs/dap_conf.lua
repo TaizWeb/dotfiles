8f1858e126e5f18c2be70f4b8ae780e869d0bb1b
taiz <walter.kerly@protonmail.com>
2024-08-13 22:14:46 -0400

    tools: Document launching a module in Python debugger

--- tool_configs/dap_conf.lua
+++ tool_configs/dap_conf.lua
@@ -1,8 +1,11 @@
 local dap_conf = {
-		type = "python",
-		request = "launch",
-		name = "Launch current file",
-		program = "test.py",
-		console = "integratedTerminal",
+	type = "python",
+	request = "launch",
+	name = "Launch current file",
+	-- The file to run
+	program = "main.py",
+	-- Or, run a module instead
+	--module = "module_name",
+	console = "integratedTerminal",
 }
 return dap_conf
